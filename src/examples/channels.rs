d33a886eed205d4a62854ba9f28e03e8974851be
angael <krzysztofwidacki@gmail.com>
2023-12-18 23:47:28 +0100

    Refactor channel communication to use a timeout for receiving messages

--- src/examples/channels.rs
+++ src/examples/channels.rs
@@ -3,15 +3,20 @@ use std::{sync::mpsc, thread};
 pub fn test_channels() {
     println!("[Channels]");
 
-    let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel::<String>();
 
     let handle1 = thread::spawn(move || {
-        let val = String::from("hi");
+        let val = String::from("message from channel thread, sending via tx");
+
+        // Comment to see the error
         tx.send(val).expect("Could not send value");
     });
 
     let handle2 = thread::spawn(move || {
-        let received = rx.recv().expect("Could not receive value");
+        let received = rx
+            .recv_timeout(std::time::Duration::from_secs(1))
+            .expect("Could not receive value");
+
         println!("Got: {}", received);
     });
 
