0571a8c6174cd8952388271a45f34d52492dbbdc
angael <krzysztofwidacki@gmail.com>
2023-12-18 23:35:31 +0100

    channels example

--- src/examples/channels.rs
+++ src/examples/channels.rs
@@ -1,3 +1,20 @@
+use std::{sync::mpsc, thread};
+
 pub fn test_channels() {
     println!("[Channels]");
+
+    let (tx, rx) = mpsc::channel();
+
+    let handle1 = thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let handle2 = thread::spawn(move || {
+        let received = rx.recv().unwrap();
+        println!("Got: {}", received);
+    });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
 }
